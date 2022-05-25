9a666d7735ef4cc899bea434d9ffc1138451c79e
Benjamin Yap <ben12190@gmail.com>
2022-05-25 12:19:48 +0800

    First Commit

---
+++ src/main/java/com/assesment/maybank/MaybankAppApplication.java
@@ -0,0 +1,13 @@
+package com.assesment.maybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaybankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaybankAppApplication.class, args);
+	}
+
+}
